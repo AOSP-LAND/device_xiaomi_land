4a6f406427729b8329cf89712beb8899f66ec449
xyyx <xyyx@mail.ru>
2017-11-18 16:50:31 +0800

    Add Marlin's apn conf

--- device.mk
+++ device.mk
@@ -30,6 +30,7 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    device/xiaomi/land/configs/apns-full-conf.xml:system/etc/apns-conf.xml \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
