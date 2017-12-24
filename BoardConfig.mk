bfa5bf769be31693ff4644e0526c68d60cdbcd15
Ritesh Saxena <riteshsax007@gmail.com>
2017-12-24 04:27:43 +0000

    land: import cryptfs_hw

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,7 @@ USE_OPENGL_RENDERER := true
 
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := $(DEVICE_PATH)/cryptfs_hw
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
