19572392dabfcaba601473074a7f0b4fbee170cf
SayanAcharya <rowdysayan99@gmail.com>
2018-01-01 10:18:06 +0000

    QCOM: Add QCOM Utils

--- aosp_land.mk
+++ aosp_land.mk
@@ -24,6 +24,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from land device
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
+# Call QCOM Utils
+$(call inherit-product, $(DEVICE_PATH)/utils.mk)
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := land
 PRODUCT_MANUFACTURER := Xiaomi
